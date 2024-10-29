06270a0ecd649e93f10c4e0deda20512f71fe400
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-10-29 16:29:31 +0300

    setup done

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+
+ return (
+  <div>
+    <h1 className='text-3xl underline' >Home Page</h1>
+    <Button>click me</Button>
+  </div>
+ )
+}
+
+ 
