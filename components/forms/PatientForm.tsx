67a497422c1f7a8321126bd634da281feac432a4
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-10-31 11:25:35 +0300

    form components and signin page

---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,99 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  
+} from "@/components/ui/form"
+
+import CustomFormFiled from "../CustomFormFiled"
+
+export enum FormFieldType{
+  INPUT ='input',
+  TEXTAREA ='textarea',
+  PHONE_INPUT='phoneInput',
+  CHECKBOX ='checkbox',
+  DATE_PICKER = 'datePicker',
+  SELECT = 'select',
+  SKELETON = 'skeleton',
+
+  
+
+  
+  
+}
+
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+const PatientForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  return(
+    <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+      <section className = "mb-12 space-y-4" >
+        <h1 className="header" >Hi their</h1>
+       <p className="text-dark-700" >schedule your appointment</p>
+
+
+      </section>
+      <CustomFormFiled
+      fieldType = {FormFieldType.INPUT}
+      control={form.control}
+
+      name="name"
+      label= "Full Name"
+      placeholder = "John Doe"
+      iconSrc="/assets/icons/user.svg"
+      iconAlt="user"
+
+      
+      />
+      <CustomFormFiled
+      fieldType = {FormFieldType.INPUT}
+      control={form.control}
+
+      name="email"
+      label= "Email"
+      placeholder = "Johndoe@gmail.com"
+      iconSrc="/assets/icons/email.svg"
+      iconAlt="email"
+
+      
+      />
+      <CustomFormFiled
+      fieldType = {FormFieldType.PHONE_INPUT}
+      control={form.control}
+
+      name="phone"
+      label= "phone number"
+      placeholder = "(254) 222-222-222"
+      
+
+      
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  </Form>
+  )
+}
+export default PatientForm
